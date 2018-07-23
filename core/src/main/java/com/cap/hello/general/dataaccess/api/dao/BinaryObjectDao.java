32758658cc9e9e262c95fdf86451bbb33024e255
Ruiz Andres <matias.ruiz-andres@capgemini.com>
2018-07-23 14:44:53 +0200

    onboarding commit

---
+++ core/src/main/java/com/cap/hello/general/dataaccess/api/dao/BinaryObjectDao.java
@@ -0,0 +1,10 @@
+package com.cap.hello.general.dataaccess.api.dao;
+
+import com.cap.hello.general.dataaccess.api.BinaryObjectEntity;
+
+/**
+ * {@link ApplicationDao Data Access Object} for {@link BinaryObjectEntity} entity.
+ */
+public interface BinaryObjectDao extends ApplicationDao<BinaryObjectEntity> {
+
+}
---
+++ core/src/main/java/com/cap/hello/general/dataaccess/base/dao/BinaryObjectDaoImpl.java
@@ -0,0 +1,20 @@
+package com.cap.hello.general.dataaccess.base.dao;
+
+import com.cap.hello.general.dataaccess.api.BinaryObjectEntity;
+import com.cap.hello.general.dataaccess.api.dao.BinaryObjectDao;
+
+import javax.inject.Named;
+
+/**
+ * Implementation of {@link BinaryObjectDao}.
+ */
+@Named
+public class BinaryObjectDaoImpl extends ApplicationDaoImpl<BinaryObjectEntity> implements BinaryObjectDao {
+
+  @Override
+  public Class<BinaryObjectEntity> getEntityClass() {
+
+    return BinaryObjectEntity.class;
+  }
+
+}
